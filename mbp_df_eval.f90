ce22cd23d121461c9bf551086064a5b5f98b11e7
Vincent Bensch <vbensch@gmail.com>
2020-05-06 23:22:11 -0500

    df clear for debug

--- mbp_df_eval.f90
+++ mbp_df_eval.f90
@@ -23,44 +23,7 @@
 
   CALL reset_ptrs
 
-  DO ielem=nelem, 1 !Loop though elements
 
-!-----------------------------------------------------------------------
-! Index pointers are advanced.
-!-----------------------------------------------------------------------
-
-    ioff = 4 * ielem
-    rptr => svec(ioff-3:ioff-2)   !Position pointer associated with current element
-    vptr => svec(ioff-1:ioff)     !Velocity pointer associated with current element
-
-    drptr => dfvec(ioff-3:ioff-2) !Derivative of position pointer associated with current element
-    dvptr => dfvec(ioff-1:ioff)   !Derivative of velocity pointer associated with current element
-
-    CALL update_rel_ptrs
-
-!-----------------------------------------------------------------------
-! The derrivative function of position is set the current 'solution' velocity
-!-----------------------------------------------------------------------
-
-    drptr(1) = vptr(1)
-    drptr(2) = vptr(2)
-
-!-----------------------------------------------------------------------
-! Compute linkage tention
-!-----------------------------------------------------------------------
-
-    rel_rptr
-
-!-----------------------------------------------------------------------
-! Evaluate the acceleration at ielem from gravitational vector, and linkage
-!-----------------------------------------------------------------------
-
-    dvptr(1) = lt_next * rel_rptr(1) / elem_rad(ielem)
-    dvptr(2) = lt_next * rel_rptr(2) / elem_rad(ielem) - grav_accel
-
-    CALL update_pre_ptrs
-
-  ENDDO
 
   RETURN
 
