173adb7d6b11161d93c4de274ae2ef363118892b
Vincent Bensch <vbensch@gmail.com>
2020-05-07 02:53:18 -0500

    Save point before expanding arrs

--- mbp_df_eval.f90
+++ mbp_df_eval.f90
@@ -18,7 +18,7 @@
   REAL(rknd), DIMENSION(nsize), TARGET, INTENT(IN) :: svec ! current ‘solution’ vector
   REAL(rknd), DIMENSION(nsize), TARGET, INTENT(OUT) :: dfvec ! will hold the derivative-function vector upon return
 
-  REAL(rknd), DIMENSION(nelem + 1, 3) :: t_elem = 0_rknd !Tention in linkage terminating in element n, and element n-1 (mag, x_comp, ycomp)
+  REAL(rknd), DIMENSION(nelem + 1, 3) :: t_elem = 0_rknd !Tention in linkage from element n to element n-1 (x_comp, ycomp, mag)
 
   REAL(rknd) :: lt_prev = 0_rknd
   REAL(rknd) :: lt_next = 0_rknd
@@ -38,7 +38,8 @@
     drptr => dfvec(ioff-3:ioff-2) !Derivative of position pointer associated with current element
     dvptr => dfvec(ioff-1:ioff)   !Derivative of velocity pointer associated with current element
 
-    CALL update_rel_ptrs
+    rptr => svec(ioff-3:ioff-2)   !Position pointer associated with current element
+    vptr => svec(ioff-1:ioff)     !Velocity pointer associated with current element
 
 !-----------------------------------------------------------------------
 ! The derrivative function of position is set the current 'solution' velocity
@@ -51,16 +52,14 @@
 ! Compute linkage tention
 !-----------------------------------------------------------------------
 
-    rel_rptr
+    
 
 !-----------------------------------------------------------------------
-! Evaluate the acceleration at ielem from gravitational vector, and linkage
+! Evaluate the acceleration at ielem from gravitational vector, and linkages
 !-----------------------------------------------------------------------
 
-    dvptr(1) = lt_next * rel_rptr(1) / elem_rad(ielem)
-    dvptr(2) = lt_next * rel_rptr(2) / elem_rad(ielem) - grav_accel
-
-    CALL update_pre_ptrs
+    dvptr(1) = (t_elem(ielem, 1) - t_elem(ielem - 1, 1)) / elem_mass(ielem)
+    dvptr(2) = (t_elem(ielem, 2) - t_elem(ielem - 1, 2)) / elem_mass(ielem) - grav_accel
 
   ENDDO
 
